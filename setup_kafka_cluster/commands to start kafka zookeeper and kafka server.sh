c2625df6bcdd17eb81907a9eef0abb24944c4616
Saurabh <saurabhjambale2001@gmail.com>
2024-02-15 08:24:15 +0530

    Saurabh

---
+++ setup_kafka_cluster/commands to start kafka zookeeper and kafka server.sh
@@ -0,0 +1,13 @@
+
+#note run both commands on two different instances of terminal
+
+#start kafka zookeper
+./run-kafka_zookeeper_server.sh -s start
+
+# start kafka server
+./run-kafka_server.sh -s start
+
+
+
+
+
